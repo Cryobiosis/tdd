9034a41adba47b0fdc3c9a83d061e28978105e31
mk <mk@morula.local>
2023-01-22 19:00:39 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -85,7 +85,7 @@ function createApp(database) {
   }
 
   function isMonday(date, date2) {
-    if (date2) { return date2.dayOfWeek === 1; }
+    return date2.dayOfWeek === 1;
     return date.getDay() === 1;
   }
 
