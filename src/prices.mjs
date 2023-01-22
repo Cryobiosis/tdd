500311eed57c5a6ca193ba7948157666529eb245
mk <mk@morula.local>
2023-01-22 18:41:38 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -78,7 +78,7 @@ function createApp(database) {
   function calculateReduction(date, date2) {
     let reduction = 0;
     if (date2 && isMonday(date, date2) && !isHoliday(date, date2)) {
-
+      reduction = 35;
     }
     if (date && isMonday(date) && !isHoliday(date)) {
       reduction = 35;
