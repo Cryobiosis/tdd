1c7db89081bea7b01c97b992303167d1e5413a41
mk <mk@morula.local>
2023-01-22 18:46:00 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -96,9 +96,9 @@ function createApp(database) {
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
       let holiday2 = Temporal.PlainDate.from(row.holiday);
-      if (date2) {}
+      if (date2 && date2.year === holiday2.year) {}
       if (
-        date &&
+        date && 
         date.getFullYear() === holiday.getFullYear() &&
         date.getMonth() === holiday.getMonth() &&
         date.getDate() === holiday.getDate()
