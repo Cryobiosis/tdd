b790ac9825ae53119d507a71d0804f1ceacb944e
mk <mk@morula.local>
2023-01-22 12:04:19 +0200

    Reset

--- src/prices.mjs
+++ src/prices.mjs
@@ -25,9 +25,6 @@ function createApp(database) {
 
   function parseDate(dateString) {
     if (dateString) {
-      console.log(dateString);
-      const date = Temporal.PlainDate.from(dateString);
-      return date;
       return new Date(dateString);
     }
   }
@@ -79,20 +76,18 @@ function createApp(database) {
   }
 
   function isMonday(date) {
-    return date.dayOfWeek === 1;
     return date.getDay() === 1;
   }
 
   function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      // let holiday = new Date(row.holiday);
-      let holiday = Temporal.PlainDate.from(row.holiday);
+      let holiday = new Date(row.holiday);
       if (
         date &&
-        date.year === holiday.year &&
-        date.month === holiday.month &&
-        date.day === holiday.day
+        date.getFullYear() === holiday.getFullYear() &&
+        date.getMonth() === holiday.getMonth() &&
+        date.getDate() === holiday.getDate()
       ) {
         return true;
       }
